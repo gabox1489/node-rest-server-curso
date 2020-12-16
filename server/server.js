ed9ad6b483145dac0cdae28f1aa7b8c2f352c027
gabriel <lb.j.23@hotmail.com>
2020-12-16 15:41:51 -0400

    Npm start listo

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require('./config/config');
 
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const bodyParser = require('body-parser');
 
@@ -10,35 +11,17 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.get('/usuario', function(req, res) {
-    res.json('get Usuario');
-});
 
-app.post('/usuario', function(req, res) {
 
-    let body = req.body;
+//ESTABLECER LA CONEXION A LA BASE DE DATOS 
 
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    } else {
-        res.json({
-            persona: body
-        });
-    }
+mongoose.connect('mongodb://127.0.0.1:27017/cafe', (err, res) => {
+    if (err) throw err;
 
+    console.log('Base de datos Online');
 });
 
 
-app.put('/usuario/:id', function(req, res) {
-
-    let id = req.params.id;
-    res.json({
-        id
-    });
-});
 
 app.listen(process.env.PORT, () => {
     console.log('Escuchando en el puerto : ', 3000);
