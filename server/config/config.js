e4c6262256b8bb7d33de23d0e722d06ef51383ec
gabriel <lb.j.23@hotmail.com>
2021-01-06 11:47:45 -0400

    Google Sing-In♥

--- server/config/config.js
+++ server/config/config.js
@@ -11,3 +11,7 @@ process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 60;
 
 //SEMILLA DE AUNTENTICACION//
 process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo';
+
+//GOOGLE CLIENT ID 
+
+process.env.CLIENT_ID = process.env.CLIENT_ID || '742528573465-2o3f1ijhg7c43lni9873ila276bg86db.apps.googleusercontent.com';
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require('./config/config');
 
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 const app = express();
 const bodyParser = require('body-parser');
 
@@ -10,6 +11,9 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
 app.use(bodyParser.json())
+
+//HABILITAR LA CARPETA PUBLIC
+app.use(express.static(path.resolve(__dirname, '../public')));
 
 
 //CONFIGURACION GLOBAL DE RUTAS
